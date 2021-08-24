e230c14efa253e415f07842f4f4e964f25edb4ad
Ben Lovy <ben@deciduously.com>
2021-08-24 15:21:23 -0400

    Some signatures

--- src/lib.rs
+++ src/lib.rs
@@ -15,19 +15,46 @@ enum SkedgeError {
     IntervalError
 }
 
+/// A Tag is used to categorize a job.
+struct Tag(String);
+
 /// A Job is anything that can be scheduled to run periodically.
-trait Job {}
+struct Job {
+    tags: Vec<Tag>
+}
 
 /// A Scheduler creates jobs, tracks recorded jobs, and executes jobs.
 struct Scheduler {
-    jobs: Vec<Box<dyn Job>>
+    jobs: Vec<Job>
 }
 
 impl Scheduler {
+    /// Instantiate a Scheduler
     fn new() -> Self {
         Self {
             jobs: Vec::new()
         }
+    }
+
+    /// Run all jobs that are scheduled to run.  Does NOT run missed jobs!
+    fn run_pending() {
+        unimplemented!()
+    }
+
+    /// Run all jobs, regardless of schedule.
+    fn run_all(delay_seconds: Option<u32>) {
+        // if None, default to 0.
+        unimplemented!()
+    }
+
+    /// Get all jobs, optionally with a given tag.
+    fn get_jobs<'a>(tag: Option<Tag>) -> &'a [Job] {
+        unimplemented!()
+    }
+
+    /// Clear all jobs, optionally only with given tag.
+    fn clear(tag: Option<Tag>) {
+        unimplemented!()
     }
 }
 
